773eb577857b4f7180a165fd3f538bcfaf636f19
Konstantin Sorokin <kvs@sigterm.ru>
2017-05-31 18:22:08 +0300

    documentation improved

--- R/zstd.R
+++ R/zstd.R
@@ -1,9 +1,22 @@
-#' Compresses character or binary vector
+#' Zstandard comression
+#'
+#' Zstandard, or zstd as short version, is a fast lossless compression algorithm, targeting real-time compression scenarios
+#' at zlib-level and better compression ratios.
+#'
+#' Compresses data using zstandard algorithm
 #'
 #' @export
-#' @param data input data to be compressed
-#' @param level compression level to use, default is 3
+#' @seealso \link{memCompress} \link{zstdDecompress} \link{zstdMaxCLevel}
+#' @references \url{http://facebook.github.io/zstd/}
+#' @param data input data to be compressed or decomressed
+#' @param level compression level to use, value between 1 and returned by \link{zstdMaxCLevel} function, default is 3
 #' @return compressed data
+#' @examples # Simple example
+#' library(zstdr)
+#' data_file <- file.path(R.home(), "COPYING")
+#' data <- readBin(data_file, raw(), file.info(data_file)$size)
+#' compressed <- zstdCompress(data)
+#' stopifnot(identical(data, zstdDecompress(compressed)))
 zstdCompress <- function(data, level=3) {
     stopifnot(is.numeric(level))
 
@@ -16,11 +29,23 @@ zstdCompress <- function(data, level=3) {
     }
 }
 
-#' Decopresses binary data
+#' Zstandard comression
+#'
+#' Zstandard, or zstd as short version, is a fast lossless compression algorithm, targeting real-time compression scenarios
+#' at zlib-level and better compression ratios.
+#'
+#' Decompresses data previously compressed with \link{zstdCompress}
 #'
 #' @export
-#' @param data compressed data to be decompressed
+#' @seealso \link{memCompress} \link{zstdCompress} \link{zstdMaxCLevel}
+#' @param data input data to be decomressed
 #' @return decompressed data
+#' @examples # Simple example
+#' library(zstdr)
+#' data_file <- file.path(R.home(), "COPYING")
+#' data <- readBin(data_file, raw(), file.info(data_file)$size)
+#' compressed <- zstdCompress(data)
+#' stopifnot(identical(data, zstdDecompress(compressed)))
 zstdDecompress <- function(data) {
     if (!is.raw(data)) {
         stop("input data must be raw")
@@ -30,7 +55,9 @@ zstdDecompress <- function(data) {
 }
 
 #' Returns maximum compression level supported by zstandard
-#
+#'
+#' @export
+#' @seealso \link{memCompress} \link{zstdCompress} \link{zstdDecompress}
 #' @return maximum supported compression level
 zstdMaxCLevel <- function() {
     return(zstdMaxCLevelImpl())
